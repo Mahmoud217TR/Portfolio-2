5be057f42b6f9f6086638a77f09b14d81faf9f50
Mahmoud217TR <faafet.mahmoud@gmail.com>
2022-02-17 17:05:14 +0200

    test

---
+++ resources/views/layouts/loader.blade.php
@@ -0,0 +1,14 @@
+<script>
+    window.addEventListener('load', (event) =>{
+        document.getElementById("loader-layer").classList.add('fade-5');
+    });
+</script>
+<div class="loader-layer" id="loader-layer">
+    <div class="d-block">
+        <span class="loader mx-auto"><span class="loader-inner"></span></span>
+    </div>
+    <div class="fulfilling-bouncing-circle-spinner">
+        <div class="circle"></div>
+        <div class="orbit"></div>
+    </div>
+</div>
